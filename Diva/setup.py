e7f75527d2614aba433ab174a127a561a31eceaa
tvmsandy33 <97993501+tvmsandy33@users.noreply.github.com>
2024-06-15 01:49:57 +0530

    Update setup.py

--- Diva/setup.py
+++ Diva/setup.py
@@ -1,20 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="your_package",
-    version="0.1",
+    name='Deeva',
+    version='0.1.0',
+    description='Contextual correction of Incorrect words using Distilber-base Backbone',
+    author='tvmsandy',
+    author_email='tvmsandy33@gmail.com',
+    url='https://github.com/tvmsandy33/Deeva',
     packages=find_packages(),
     install_requires=[
-        "pandas",
-        "numpy",
-        "datasets",
-        "torch",
-        "transformers",
-        "spacy"
+        'pandas',
+        'numpy',
+        'datasets',
+        'torch',
+        'transformers',
+        'spacy',
     ],
-    entry_points={
-        'console_scripts': [
-            'your_package=your_package:main',
-        ],
-    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
